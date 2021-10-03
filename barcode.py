32ad7c4ec4bf38ad011194a4cc36b697a9f68266
Henry Pearson <62584880+A9-dev@users.noreply.github.com>
2021-10-03 13:49:56 +0100

    Add files via upload

---
+++ barcode.py
@@ -0,0 +1,41 @@
+import cv2
+from pyzbar import pyzbar
+
+last_barcode = ""
+
+
+def decode(image):
+    global last_barcode
+    # decodes all barcodes from an image
+    decoded_objects = pyzbar.decode(image)
+    for obj in decoded_objects:
+        if obj.data != last_barcode:
+            last_barcode = obj.data
+            print(last_barcode)
+    return image
+
+
+if __name__ == "__main__":
+
+    cv2.namedWindow("preview")
+    vc = cv2.VideoCapture(0)
+
+    if vc.isOpened():  # try to get the first frame
+        rval, frame = vc.read()
+    else:
+        rval = False
+
+    while rval:
+        cv2.imshow("preview", frame)
+        img = decode(frame)
+        rval, frame = vc.read()
+        key = cv2.waitKey(20)
+        if key == 27:  # exit on ESC
+            break
+
+    vc.release()
+    cv2.destroyWindow("preview")
+
+    # show the image
+    cv2.imshow("img", img)
+    cv2.waitKey(0)
